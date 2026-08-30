bbac17fa67d17b59a799f995f4057c4b74782cde
NalenSrin123 <srinnalen@gmail.com>
2026-08-30 12:22:34 +0700

    rollback env

---
+++ database/migrations/2026_08_30_014537_create_tour_schedule_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tour_schedule', function (Blueprint $table) {
+            $table->id('schedule_id');
+
+            $table->foreignId('tour_id')
+                ->constrained('tours')
+                ->cascadeOnDelete();
+
+            $table->foreignId('guide_id')
+                ->constrained('guides')
+                ->cascadeOnDelete();
+
+            $table->date('start_date');
+            $table->date('end_date');
+
+            $table->unsignedInteger('max_capacity');
+            $table->unsignedInteger('booked_seats')->default(0);
+
+            $table->enum('status', ['UPCOMING', 'COMPLETED'])
+                ->default('UPCOMING');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tour_schedule');
+    }
+};

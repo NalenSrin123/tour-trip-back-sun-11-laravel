50f0ef2d512147bf9b3af12015008f0506782b46
Pen Bora <penbora97@gmail.com>
2026-09-06 08:27:59 +0700

    fix role Controler and route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Api\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-Route::apiResource('roles', RoleController::class);
+Route::get('/roles', [RoleController::class, 'index']);
+Route::post('/roles', [RoleController::class, 'store']);
+Route::get('/roles/{id}', [RoleController::class, 'show']);
+Route::put('/roles/{id}', [RoleController::class, 'update']);
+Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
